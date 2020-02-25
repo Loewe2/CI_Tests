396176a9a6aba99f04a7adbc0eef3de0a5570fae
arkantos493 <b-marci@web.de>
2020-02-25 21:11:18 +0100

    Update install_gcc.sh

--- .github/workflows/install_gcc.sh
+++ .github/workflows/install_gcc.sh
@@ -1,1 +1,10 @@
-echo "From script file $1"
+sudo apt-get install flex
+git clone https://github.com/gcc-mirror/gcc.git
+cd gcc
+./contrib/download_prerequisites
+cd ..
+mkdir objdir
+cd objdir
+$PWD/../gcc/configure --prefix="$1" --enable-languages=c,c++ --disable-multilib
+make
+make install
